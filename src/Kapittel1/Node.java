05f0afcea76c02747c2d25ce6ee7f8e6789228bc
Ole Eskild <oleeskilds@gmail.com>
2014-09-02 17:21:42 +0200

    Added linkedStack

---
+++ src/Kapittel1/Node.java
@@ -0,0 +1,11 @@
+package Kapittel1;
+
+public class Node {
+
+	private final String data;
+	public Node next;
+	
+	public Node(String data){
+		this.data = data;
+	}
+}
--- src/Kapittel1/Oppgave121.java
+++ src/Kapittel1/Oppgave121.java
@@ -2,21 +2,25 @@ package Kapittel1;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 import edu.princeton.cs.introcs.StdRandom;
 
 public class Oppgave121 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int N = Integer.parseInt(args[0]);
 		Point2D[] points = new Point2D[N];
+		
+		//Creates random points in unit square and draws them to visualize it
 		for(int i = 0; i<N; i++){
 			points[i] = new Point2D(StdRandom.uniform(), StdRandom.uniform());
 			StdDraw.point(points[i].x(), points[i].y());
 		}
-		//Finding the closes pair of points
+		
+		//Finding the closest pair of points
 		double leastDist = 1;
+		//Need this to draw a line between the closest points
 		Point2D[] closestPoint = new Point2D[2];
 		for(int i = 0; i<points.length;i++){
 			for(int j = 0; j<points.length;j++){
@@ -27,15 +31,18 @@ public class Oppgave121 {
 				}
 			}
 		}
+		//Prints out the distance of the closest points, then draws a line connecting the two
 		StdOut.println(leastDist);
 		StdDraw.line(closestPoint[0].x(), closestPoint[0].y(), closestPoint[1].x(), closestPoint[1].y());
 
 	}
 	
+	//Calculate the distance between to points
 	private static double getDistance(Point2D point1, Point2D point2){
-		double x = point1.x() - point2.x();
-		double y = point1.y() - point2.y();
-		return Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2))); 
+//		double x = point1.x() - point2.x();
+//		double y = point1.y() - point2.y();
+//		return Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2))); 
+		return point1.distanceTo(point2);
 	}
 
 }
